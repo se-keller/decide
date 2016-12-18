dd99222a951c65ef6e4df11be7852ebdf3fbe8f3
se-keller <sebastian.sekell@googlemail.com>
2016-12-19 00:00:24 +0100

    Accept and Agree buttons. Refactoring.

---
+++ js/consent/consent-create.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $('#txtarea-consent-proposal').on('input propertychange paste', function() {
+      if($('#txtarea-consent-proposal').val()==="") {
+        $('#btn-consent-proposal-accept').addClass("disabled")
+        $('#btn-consent-proposal-agree').addClass("disabled")  
+      } else {
+        $('#btn-consent-proposal-accept').removeClass("disabled")
+        $('#btn-consent-proposal-agree').removeClass("disabled")  
+      }
+  });
+});
--- parts/build.sh
+++ parts/build.sh
@@ -1,3 +1,7 @@
+echo
+
+set -e
+
 remove() {
 	if test -e "$1"
 		then
@@ -8,20 +12,22 @@ remove() {
 checkIfCreated() {
 	if test -e "$1"
 	then
-		echo DONE $1 concatinated
+		echo [DONE] $1 concatinated
 	else
-		echo FAIL coould not create $1
-fi
+		echo [FAIL] coould not create $1
+	fi
 }
 
 file="../index.html"
 remove $file
-cat head.part.html navigation.part.html index.part.html bottom-script.part.html > $file
+cat head.part.html close-head.part.html navigation.part.html index.part.html close-body.part.html > $file
 checkIfCreated $file
 
 
-file="../create-consent.html"
+file="../consent-create.html"
 remove $file
-cat head.part.html navigation.part.html create/create-consent.part.html bottom-script.part.html > $file
+cat head.part.html consent/scripts.part.html close-head.part.html navigation.part.html consent/consent-create.part.html close-body.part.html > $file
 checkIfCreated $file
+
+echo 
 
