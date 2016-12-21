998e5bf611c7b6336a4bd8705fd13cb2927643cc
se-keller <sebastian.sekell@googlemail.com>
2016-12-22 00:26:49 +0100

    Google Login Try

--- js/google/oauth.js
+++ js/google/oauth.js
@@ -6,7 +6,7 @@ function OAuth() {
   var DISCOVERY_URL = ["https://sheets.googleapis.com/$discovery/rest?version=v4", "https://people.googleapis.com/$discovery/rest?version=v1"]
   var API_KEY = 'AIzaSyDbR2kJv9QUCbSRPOPt3R7v31NCquDEz7w';
   var instance = this;
-  console.log("- userinfo not yet");
+  console.log("- userinfo under the hood");
 
   this.start = function() {
     gapi.load('client:auth2', initClient);
@@ -24,6 +24,7 @@ function OAuth() {
         gapi.auth2.getAuthInstance().signIn().then(
           function(response){
             console.log("Log in successful");
+            JSON.stringify(response)
             createConsent()
             getUserInfo()
           }, function(response){
