574a2de02b7e88551a28694db9b0b1a019d3955c
se-keller <sebastian.sekell@googlemail.com>
2016-12-21 16:36:56 +0100

    Google Sign In Try

--- js/google/oauth.js
+++ js/google/oauth.js
@@ -2,11 +2,11 @@ function OAuth() {
 
   var authorized = true;
   var CLIENT_ID = '847560978980-gj7ac8oo7h5spk4uupdko3j865aon6hu.apps.googleusercontent.com';
-  var SCOPES = 'people https://www.googleapis.com/auth/spreadsheets'
+  var SCOPES = 'profile https://www.googleapis.com/auth/spreadsheets'
   var DISCOVERY_URL = ["https://sheets.googleapis.com/$discovery/rest?version=v4", "https://people.googleapis.com/$discovery/rest?version=v1"]
   var API_KEY = 'AIzaSyDbR2kJv9QUCbSRPOPt3R7v31NCquDEz7w';
   var instance = this;
-  console.log("- 12");
+  console.log("- 13");
 
   this.start = function() {
     gapi.load('client:auth2', initClient);
