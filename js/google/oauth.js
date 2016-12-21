521c5b0abb1c4145972105b87256ce5b8823ecd6
se-keller <sebastian.sekell@googlemail.com>
2016-12-21 15:19:06 +0100

    Google Sign In Try

--- js/google/oauth.js
+++ js/google/oauth.js
@@ -3,6 +3,7 @@ function OAuth() {
   var authorized = true;
 
   this.authorize = function() {
+    console.log("- 1")
       gapi.auth.authorize({
         client_id: '847560978980-gj7ac8oo7h5spk4uupdko3j865aon6hu.apps.googleusercontent.com', 
         scope: "https://www.googleapis.com/auth/spreadsheets", 
