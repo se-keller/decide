7a7b12b321e2da1ed148843244b4f7a240d2bcc0
se-keller <sebastian.sekell@googlemail.com>
2016-12-23 23:59:52 +0100

    show invite link on share html

--- js/share/share.js
+++ js/share/share.js
@@ -2,6 +2,6 @@ $(document).ready(function() {
 	var urlParamsDecoder = new UrlParamsDecoder(window.location.href)
     if(urlParamsDecoder.hasParams()) {
         var id = urlParamsDecoder.valueOf("id")
-        $('#invite-url').text(window.location.origin + "/decide?id="+id)
+        $('#invite-url').val(window.location.origin + "/decide?id="+id)
     }
 })
