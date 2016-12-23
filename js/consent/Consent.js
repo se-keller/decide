7a800e0ef2e7570d3baecb0e739f726ee18a6e59
se-keller <sebastian.sekell@googlemail.com>
2016-12-23 17:36:11 +0100

    save consent object

--- js/consent/Consent.js
+++ js/consent/Consent.js
@@ -2,18 +2,22 @@ function Consent(creator, proposal) {
 	var agreeCount = 0
 	var acceptCount = 0
 	var currentDecision = proposal
+	
 	this.agree = function() {
 		agreeCount++
 	}
+	
 	this.accept = function() {
 		acceptCount++
 	}
+	
 	this.disagree = function(reason, newProposal) {
 		agreeCount = 0
 		acceptCount = 0
 		currentDecision = newProposal
 	}
-	this.currentDecision() {
+
+	this.currentDecision = function() {
 		return currentDecision
 	}
 }
