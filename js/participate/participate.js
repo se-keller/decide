69c3768face3a08f35c7e2a4babc6e008dab33bf
se-keller <sebastian.sekell@googlemail.com>
2016-12-28 00:28:20 +0100

    participate find entry

--- js/participate/participate.js
+++ js/participate/participate.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
+	console.log('Durchstich')
 	var urlParamsDecoder = new UrlParamsDecoder(window.location.href)
     if(urlParamsDecoder.hasParams()) {
         var id = urlParamsDecoder.valueOf("id")
         var gSheet = new GSheets('1bsPVDw_DKoByu3_y8bn3pQ_VAF8Mr8QJA5pcZIZATpI')
         console.log(gSheet.findRow(0, id))
     } 
-}
+})
