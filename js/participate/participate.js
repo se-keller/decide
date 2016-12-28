c38bbede49d6a721b9836d036538b30af3a65e6e
se-keller <sebastian.sekell@googlemail.com>
2016-12-29 00:27:09 +0100

    refactor async calls

--- js/participate/participate.js
+++ js/participate/participate.js
@@ -2,8 +2,7 @@ $(document).ready(function() {
 	var oauth = new OAuth()
   	oauth.login(function(){
   		var urlParamsDecoder = new UrlParamsDecoder(window.location.href)
-    	if(urlParamsDecoder.hasParam('id2')) {
-    		console.log("id2")
+    	if(urlParamsDecoder.hasParam('id')) {
        		var id = urlParamsDecoder.valueOf('id')
         	var gSheet = new GSheets(DECIDE_REPOSITORY_GOOGLE_SPREADSHEET_ID)
         	gSheet.findRow(0, id, function(result){
