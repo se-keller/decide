80cf337502c8db369a36fa348f72dcd9e13ebc5b
se-keller <sebastian.sekell@googlemail.com>
2016-12-29 00:01:16 +0100

    parse the decision

--- js/participate/participate.js
+++ js/participate/participate.js
@@ -12,6 +12,7 @@ $(document).ready(function() {
         	var gSheet = new GSheets(DECIDE_REPOSITORY_GOOGLE_SPREADSHEET_ID)
         	gSheet.findRow(0, id, function(result){
         		console.log(JSON.parse(result[3]).type)
+        		window.location.href = result[3].type + '-participate.html?id=' + id
         	})
     	} 
 	});
