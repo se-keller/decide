679dccf4213f3260a13fc69698b832995762ebf4
se-keller <sebastian.sekell@googlemail.com>
2016-12-30 00:04:55 +0100

    refactor to choosable sheet

--- js/participate/participate.js
+++ js/participate/participate.js
@@ -1,13 +1,14 @@
+var consentRepository
 $(document).ready(function() {
+	consentRepository = new ConsentRepository()
 	var oauth = new OAuth()
   	oauth.login(function(){
   		var urlParamsDecoder = new UrlParamsDecoder(window.location.href)
     	if(urlParamsDecoder.hasParam('id')) {
        		var id = urlParamsDecoder.valueOf('id')
-        	var gSheet = new GSheets(DECIDE_REPOSITORY_GOOGLE_SPREADSHEET_ID)
-        	gSheet.findRow(0, id, function(result){
-        		window.location.href = JSON.parse(result[3]).type + '-participate.html?id=' + id
-        	})
+       		consentRepository.find(id, function() {
+       			window.location.href = JSON.parse(result[3]).type + '-participate.html?id=' + id
+       		})
     	} 
   	})
 })
