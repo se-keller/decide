3f83d1f4769c5f72ea5243535750aafa7794a2c9
se-keller <sebastian.sekell@googlemail.com>
2017-01-04 01:02:31 +0100

    disagree on consent

--- js/consent/consent-disagree.js
+++ js/consent/consent-disagree.js
@@ -10,11 +10,13 @@ $(document).ready(function() {
   var login = new Login()
   login.login(function(){
     profile = new Profile()
+    console.log("profile found")
     $('#img-consent-disagree-creator').attr('src', profile.imageUrl)
     var urlParamsDecoder = new UrlParamsDecoder(window.location.href)
   	if(urlParamsDecoder.hasParam('id')) {
    		var id = urlParamsDecoder.valueOf('id')
       consentRepository.find(id, function(consent) {
+      	console.log("consent found")
       	disagreeConsent = consent
       })
   	}
@@ -44,12 +46,16 @@ $(document).ready(function() {
 
 	$('#btn-consent-proposal-disagree-agree').click(function(){
     if(isEnabled('#btn-consent-proposal-disagree-accept')) {
-    	consent.disagree(profile.email, $('#txtarea-consent-disagree-proposal').text(), $('#txtarea-consent-disagree-reason').text())
-        consent.agree(profile.email, $('#txtarea-consent-proposal').val())
-        consentRepository.persist(consent, function(){
-          window.location.href = 'consent-participate.html?id=' + consent.uuid
-        })  
-    }
-  });
+	    	consent.disagree(profile.email, $('#txtarea-consent-disagree-proposal').text(), $('#txtarea-consent-disagree-reason').text())
+	        consent.agree(profile.email, $('#txtarea-consent-proposal').val())
+	        consentRepository.persist(consent, function(){
+	          window.location.href = 'consent-participate.html?id=' + consent.uuid
+	        })  
+    	}
+  	});
+
+  	$('#btn-consent-disagree-proposal-cancel').click(function(){
+    	window.location.href = 'consent-participate.html?id=' + consent.uuid
+ 	})
 
 }
