127e5bcb9869f9b39acbf067dc67239390bd4701
se-keller <sebastian.sekell@googlemail.com>
2017-01-07 23:46:58 +0100

    refactoring consent history

--- js/consent/consent-participate.js
+++ js/consent/consent-participate.js
@@ -21,87 +21,7 @@ $(document).ready(function() {
   	} 
 	})
 
-  var refreshConsent = function(consent) {
-    $('#p-consent-participate-current-decision').html(consent.currentProposal().replace(/(?:\r\n|\r|\n)/g, '<br />'))
-    refreshBadges(consent)
-    refreshButtons(consent)
-    profileRepository.find(consent.creator(), function(creatorProfile){
-      $('#img-consent-participate-creator').attr('src', creatorProfile.imageUrl)
-      $('#h-consent-participate').empty()
-    $('#h-consent-participate').append(creatorProfile.givenName+ ' <small><i>'+new Date(consent.currentProposalDate()).toLocaleString()+'</i></small>')
-    }, function(){
-      console.log('Profile of creator not found')
-    })
-    $('#consent-history-body').empty()
-    
-    $.each(consent.votes, function(index, vote){
-      profileRepository.find(vote.voter, function(profile){
-        $('#consent-history-body').append(createVoteHtml(vote, profile))
-      }, function() {
-        console.log('Profile of voter not found')
-      })
-      
-    })
-  }
-
-  var createVoteHtml = function(vote, callback) {
-    
-    profileRepository.find(vote.voter, function(profile){
-    var voteHtml = ''
-    if(vote.vote === 'disagree') {
-      voteHtml = '<div class="panel panel-danger">'
-      + '<div class="panel-heading">'
-      +   '<h3 class="panel-title">New proposal</h3>'
-      + '</div>'
-    } 
-
-    if(vote.vote !== 'disagree' && vote.proposal != undefined) {
-      voteHtml = '<div class="panel panel-default">'
-      + '<div class="panel-heading">'
-      +   '<h3 class="panel-title">Initial proposal</h3>'
-      + '</div>'
-    }
-
-    voteHtml += 
-        '<div class="panel-body">'
-      +  '<div class="media">'
-      +   '<div class="media-left">'
-      +     '<img class="media-object img-circle" src="'+profile.imageUrl+'" >'
-      +   '</div>'
-      +   '<div class="media-body">'
-      +     '<h4 class="media-heading">'+profile.givenName+' <small><i>'+new Date(vote.voteDate).toLocaleString()+'</i></small></h4>'
-      
-      if(vote.vote === 'agree')
-        voteHtml += '<div><span class="glyphicon glyphicon glyphicon-thumbs-up" aria-hidden="true"></span></div>'
-      if(vote.vote === 'accept')
-        voteHtml += '<div><span class="glyphicon glyphicon glyphicon-thumbs-up gly-rotate-270" aria-hidden="true"></span></div>'
-      if(vote.vote === 'disagree')
-        voteHtml += '<div><span class="glyphicon glyphicon glyphicon-thumbs-down" aria-hidden="true"></span></div>'
-      
-      
-
-      if(vote.reason!=undefined && vote.reason !== '') {
-        voteHtml += '<b>Reason</b><br/>'+vote.reason+'<br/><br/>'
-      }
-
-      if(vote.vote === 'disagree') {
-        voteHtml += '<b>Proposal</b><br/>'
-      }
-
-      if(vote.proposal!= undefined)
-        voteHtml += vote.proposal
-
-      voteHtml +=
-          '</div>'
-      +  '</div>'
-      + '</div>'
-
-      if(vote.proposal != undefined)
-        voteHtml += '</div>'
-    callback(voteHtml)
-    }, function(){console.log('Profile of voter not found')})
-    
-  }
+  
 
   $('#btn-consent-participate-agree').click(function(){
     if(isEnabled('#btn-consent-participate-agree')) {
@@ -152,9 +72,31 @@ $(document).ready(function() {
   $('#btn-consent-participate-disagree').click(function(){    
     $("#modal-consent-disagree").modal('show');
   })
-  
 
 })
+
+function refreshConsent(consent) {
+    $('#p-consent-participate-current-decision').html(consent.currentProposal().replace(/(?:\r\n|\r|\n)/g, '<br />'))
+    refreshBadges(consent)
+    refreshButtons(consent)
+    profileRepository.find(consent.creator(), function(creatorProfile){
+      $('#img-consent-participate-creator').attr('src', creatorProfile.imageUrl)
+      $('#h-consent-participate').empty()
+    $('#h-consent-participate').append(creatorProfile.givenName+ ' <small><i>'+new Date(consent.currentProposalDate()).toLocaleString()+'</i></small>')
+    }, function(){
+      console.log('Profile of creator not found')
+    })
+    $('#consent-history-body').empty()
+    
+    $.each(consent.votes, function(index, vote){
+      profileRepository.find(vote.voter, function(profile){
+        $('#consent-history-body').append(createVoteHtml(vote, profile))
+      }, function() {
+        console.log('Profile of voter not found')
+      })
+      
+    })
+  }
 
 function refreshBadges(consent) {
   $('#bdg-consent-participate-agree').text(consent.agreeCount())
