8463c9bf4fa655ea11258fdf9fdcd363303d9575
se-keller <sebastian.sekell@googlemail.com>
2017-01-07 23:06:48 +0100

    profile cache

--- js/profile/ProfileRepository.js
+++ js/profile/ProfileRepository.js
@@ -1,4 +1,5 @@
 function ProfileRepository() {
+  var cache = {}
 	var EMAIL_COLUMN = 0;
 	var JSON_OBJECT_COLUMN = 1;
 
@@ -10,21 +11,27 @@ function ProfileRepository() {
   				persistedCallback()
   			}, function(){
   				gSheet.append('profiles', [ [profile.email, JSON.stringify(profile)] ], function(){
+            cache[profile.email] = profile
   					persistedCallback()
   				})	
-  			})
-
-  		
+  			})  
+        		
 	}
 
 	this.find = function(id, foundCallback, notFoundCallback) {
-		var gSheet = new GSheets(DECIDE_REPOSITORY_GOOGLE_SPREADSHEET_ID)
-    	gSheet.findRow('profiles', EMAIL_COLUMN, id, 
-    		function(result){
-    			var profile = JSON.parse(result[JSON_OBJECT_COLUMN])
-        		foundCallback(profile)
-    		}, function(){
-    			notFoundCallback()
-    		})
+    if(id in cache) {
+      foundCallback(cache[id])
+    } else {
+      var gSheet = new GSheets(DECIDE_REPOSITORY_GOOGLE_SPREADSHEET_ID)
+      gSheet.findRow('profiles', EMAIL_COLUMN, id, 
+        function(result){
+          var profile = JSON.parse(result[JSON_OBJECT_COLUMN])
+            cache[id] = profile  
+            foundCallback(profile)
+        }, function(){
+          notFoundCallback()
+        })  
+    }
+		
 	}
 }
