35093ee0e37479e0ea67ba3df799fe511cea58a6
se-keller <sebastian.sekell@googlemail.com>
2017-01-11 00:58:28 +0100

    clean login

--- test/unit/mocks/google/OAuth-Mock.js
+++ test/unit/mocks/google/OAuth-Mock.js
@@ -1,10 +1,18 @@
 function OAuth() {
 
+  var isLoggedIn = false
+
+  this.isLoggedIn = function(callback) {
+    callback(isLoggedIn)
+  }
+
   this.login = function(callback) {
+  	isLoggedIn = true
     callback()
   }
 
   this.logout = function(callback) {
+  	this.isLoggedIn = false
     callbck()
   }
 
