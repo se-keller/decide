a3958be615894dd3b680468c2a86121185062188
se-keller <sebastian.sekell@googlemail.com>
2017-01-11 15:12:42 +0100

    logout

---
+++ js/navigation.js
@@ -0,0 +1,7 @@
+var refreshNavigation = function(profile) {
+	$('#nav-user-image').attr('src', profile.imageUrl)
+    $('#nav-profile-user-image').attr('src', profile.imageUrl)
+    $('#nav-profile-user-name').text(profile.givenName + ' ' + profile.familyName)
+    $('#nav-profile-user-email').text(profile.email)
+    $('#nav-drpdwn-menu').removeClass('hidden')
+}
