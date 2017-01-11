26587c60e7f5df8c78afb1351bb0e6b55333af55
se-keller <sebastian.sekell@googlemail.com>
2017-01-11 14:34:49 +0100

    clean login

--- js/start-deciding.js
+++ js/start-deciding.js
@@ -1,18 +1,24 @@
 $(document).ready(function() {
+	var redirect = 'consent-create.html'
+	var urlParamsDecoder = new UrlParamsDecoder(window.location.href)
+  	if(urlParamsDecoder.hasParam('login')) {
+   		redirect = urlParamsDecoder.valueOf('login')
+  	} 
 
   	$('#btn-start-deciding').click(function(){
   		console.log('Login...')
     	login.isLoggedIn(function(loggedIn) {
     		console.log('login status = ' + loggedIn)
     		if(loggedIn) {
-    			window.location.href = 'consent-create.html'
+    			window.location.href = redirect
     		} else {
     			// could login
     			login.login(function() {
-    				window.location.href = 'consent-create.html'
+    				window.location.href = redirect
     			// could not login
     			}, function(){
     				$("#alrt-no-login").removeClass("hidden")
+    				$("#alrt-no-login").show()
     			})
     		}
     	})
