50020dece6643404abd12dad826d5a6baf2b07b6
se-keller <sebastian.sekell@googlemail.com>
2017-01-11 17:37:45 +0100

    dashboard

--- js/dashboard/decisions-table.js
+++ js/dashboard/decisions-table.js
@@ -1,3 +1,12 @@
-var createConsentsTableBody = function() {
-
+var createConsentsTableBody = function(consents) {
+	var html = ''
+	$.each(consents, function(index, consent){
+		if(consent.type === 'consent')
+			html += '<tr>'
+			+    '<td>' + consent.type + '</td>'
+			+    '<td>' + consent.currentProposal() + '</td>'
+			+    '<td><a href="consent-participate.html?id=' + consent.uuid + '"">participate</a></td>'
+			+ '</td>'
+		
+	})
 }
