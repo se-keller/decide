628c2ca348ec1892279e6d897785f3246e4a70b2
se-keller <sebastian.sekell@googlemail.com>
2017-01-11 14:03:48 +0100

    clean login

--- js/start-deciding.js
+++ js/start-deciding.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
   	$('#btn-start-deciding').click(function(){
   		console.log('Login...')
-    	login.isLoggedIn(function(loggedIn)) {
+    	login.isLoggedIn(function(loggedIn) {
     		console.log('login status = ' + loggedIn)
     		if(loggedIn) {
     			window.location.href = 'consent-create.html'
