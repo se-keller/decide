d724fdb9a113089d277c2729da9a482cc890c42f
se-keller <sebastian.sekell@googlemail.com>
2017-01-12 14:39:58 +0100

    dashboard

--- js/navigation.js
+++ js/navigation.js
@@ -4,7 +4,8 @@ var refreshNavigation = function(profile) {
     $('#nav-profile-user-name').text(profile.givenName + ' ' + profile.familyName)
     $('#nav-profile-user-email').text(profile.email)
     $('#nav-drpdwn-menu').removeClass('hidden')
-    
+    $('#nav-dashboard').removeClass('hidden')
+
     $('#btn-logout').click(function(){
     	login.logout(function(){
     		window.location.href = 'index.html?logout=true'
