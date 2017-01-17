faddc12a74682dfdcd555b3073ad5a72edf2cf83
se-keller <sebastian.sekell@googlemail.com>
2017-01-17 01:10:47 +0100

    better login

--- js/start-deciding.js
+++ js/start-deciding.js
@@ -14,7 +14,6 @@ $(document).ready(function() {
   	$('#btn-start-deciding').click(function(){
   		console.log('Login...')
     	login.isLoggedIn(function(loggedIn) {
-    		console.log('login status = ' + loggedIn)
     		if(loggedIn) {
     			window.location.href = redirect
     		} else {
